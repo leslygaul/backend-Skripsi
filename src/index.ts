cac2fc4a50d1b92251b76f452b1983b4b829960a
leslygaul <92374932+leslygaul@users.noreply.github.com>
2025-06-15 00:05:41 +0700

    Fix Error Deploy Vercel

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,19 @@ import routes from './routes'
 const app = express()
 const PORT = process.env.PORT
 
+const allowedOrigins = process.env.ORIGINS?.split(',');
+
 app.use(cors({
-  origin: process.env.ORIGIN,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
   credentials: true,
-}))
+}));
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(compression())
